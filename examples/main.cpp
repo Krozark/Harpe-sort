bf2fe568bc8bcb3103557b1eedf08a7315cdd819
Krozark <maxime.barbier1991@gmail.com>
2014-03-28 16:51:45 +0100

    add include and examples

---
+++ examples/main.cpp
@@ -0,0 +1,9 @@
+#include <harpe-sort/calc_sort.hpp>
+#include <iostream>
+
+int main(int argc,char* argv[])
+{
+    double values[20] = {0};
+    std::cout<<calc_score(values)<<std::endl;
+    return 0;
+}
