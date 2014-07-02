d582f0d48cd083386c057eb41f92b44d1393be8c
Krozark <maxime.barbier1991@gmail.com>
2014-07-02 12:09:41 +0200

    update help

--- src/harpe-sort-learning/main.cpp
+++ src/harpe-sort-learning/main.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define SHOW_ARGS(x) {cout<<x<<endl\
     <<"\t -h, -help, montre ce message"<<endl\
     <<"\t -f mgf input file (obligatoire)"<<endl\
-    <<"\t -t mgf input test file (optional)"<<endl\
+    <<"\t -t mgf input test file (optional) you can add multiple files names using \",\" as separator"<<endl\
     <<"\t -pop-total (defaut = 1000) population"<<endl\
     <<"\t -pop-enf (defaut = 0.8) .population d'enfants. if value is [0~1[,precent of [pop-total], else the number"<<endl\
     <<"\t -mutation (defaut = 1 %) [entre 0 et 100]) taux de mutation"<<endl\
