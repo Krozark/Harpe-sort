c11f9f6b5d1b8aa13a6b142d19a2662d6b74ac4b
Krozark <maxime.barbier1991@gmail.com>
2014-07-30 00:25:41 +0200

    remove gauss

--- src/harpe-sort/calc_sort.cpp
+++ src/harpe-sort/calc_sort.cpp
@@ -9,8 +9,24 @@
 #define gauss utils::maths::ker::gaussian
 
 extern "C" {
-double calc_score(const double* const vals)
-{
-	return (gauss((gauss((((vals[6])/(vals[2]))/(((sin(sin(vals[3])))-((sin(((((vals[6])/(vals[2]))/((vals[4])*(vals[5])))+(gauss(vals[7])))-(vals[3])))-((vals[6])/(gauss((vals[4])*(vals[5]))))))*(vals[5])))+((((vals[6])+(vals[3]))+(0.246167))-(sin(0.547814)))))*((((0.502697)-((0.823913)/(vals[7])))+(0.0812427))/(sin(0.246167)))));
-}
+    double calc_score(const double* const vals)
+    {
+        return (
+            gauss((gauss(
+                (((vals[6])/(vals[2])) //NB_AA / INTENSITEE_TOTAL_PARENT
+                 /(
+                     ((sin(sin(vals[3]))) //INTENSITEE_TOTAL
+                      -((sin((
+                          (((vals[6])/(vals[2]))/((vals[4])*(vals[5])))+(gauss(vals[7])) //(NB_AA / INTENSITEE_TOTAL_PARENT)/(MASS_TOTAL*PERCENT_COUVERTURE)+gauss(MASS_PARENT)
+
+                      )-(vals[3]) //INTENSITEE_TOTAL_PARENT
+                      ))-((vals[6])/(gauss((vals[4])*(vals[5])))) //PERCENT_COUVERTURE / gauss(MASS_TOTAL*PERCENT_COUVERTURE)
+                      ))*(vals[5])//PERCENT_COUVERTURE
+                 ))
+                +((((vals[6])+(vals[3]))+(0.246167))-(sin(0.547814)))//NB_AA+INTENSITEE_TOTAL + C
+            ))
+                *((((0.502697)-((0.823913)/(vals[7])))+(0.0812427))/(sin(0.246167)) //MASS_PARENT
+                 )
+            ));
+    }
 }
