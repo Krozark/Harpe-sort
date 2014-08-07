dedb3bb66ae0e10cae66509cc16b9cf60c87bf99
Krozark <maxime.barbier1991@gmail.com>
2014-08-07 19:17:44 +0200

    change sort func

--- src/harpe-sort/calc_sort.cpp
+++ src/harpe-sort/calc_sort.cpp
@@ -1,32 +1,16 @@
 /*
-*	date: 25-07-2014 13:06
-*	score: 0.433022
-*	size: 52
-*	generation: 15
+*	date: 07-08-2014 17:25
+*	score: 0.365323
+*	size: 79
+*	generation: 20
 *	island: 2
 */
 #include <utils/maths.hpp>
 #define gauss utils::maths::ker::gaussian
 
 extern "C" {
-    double calc_score(const double* const vals)
-    {
-        return (
-            gauss((gauss(
-                (((vals[6])/(vals[2])) //NB_AA / INTENSITEE_TOTAL_PARENT
-                 /(
-                     ((sin(sin(vals[3]))) //INTENSITEE_TOTAL
-                      -((sin((
-                          (((vals[6])/(vals[2]))/((vals[4])*(vals[5])))+(gauss(vals[7])) //(NB_AA / INTENSITEE_TOTAL_PARENT)/(MASS_TOTAL*PERCENT_COUVERTURE)+gauss(MASS_PARENT)
-
-                      )-(vals[3]) //INTENSITEE_TOTAL_PARENT
-                      ))-((vals[6])/(gauss((vals[4])*(vals[5])))) //PERCENT_COUVERTURE / gauss(MASS_TOTAL*PERCENT_COUVERTURE)
-                      ))*(vals[5])//PERCENT_COUVERTURE
-                 ))
-                +((((vals[6])+(vals[3]))+(0.246167))-(sin(0.547814)))//NB_AA+INTENSITEE_TOTAL + C
-            ))
-                *((((0.502697)-((0.823913)/(vals[7])))+(0.0812427))/(sin(0.246167)) //MASS_PARENT
-                 )
-            ));
-    }
+double calc_score(const double* const vals)
+{
+	return ((vals[0])-((((vals[5])-((0.333492)-((((vals[1])+(0.00579943))-(vals[3]))/((((0.68291)+(0.966582))*((vals[1])+(0.393935)))+(vals[8])))))/((((0.68291)+(0.551302))*((vals[1])+(0.393935)))+(((((0.00579943)-(((vals[3])-(0.20546))-(((vals[5])-((0.333492)-((((vals[1])+(0.00579943))-(vals[3]))/((((0.68291)+(((0.68291)+(0.966582))*((vals[1])+(0.393935))))*((vals[1])+(0.393935)))+(vals[8])))))-(vals[3]))))/(((vals[8])*((vals[1])+(0.766061)))+(vals[1])))/(0.333492))/(vals[7]))))*((vals[7])*(vals[6]))));
+}
 }
